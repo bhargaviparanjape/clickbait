40355b5b207f41d1753dbbbce5298db8006cd593
U-FAREAST\t-bhpara <t-bhpara@T-BHPARA-DT.fareast.corp.microsoft.com>
2016-12-14 13:44:41 +0530

    code changes

--- server.py
+++ server.py
@@ -9,12 +9,13 @@ import utility
 
 
 ###############Classifier############################
+print "creating classifier..."
 no_samples = 10000
 positive = numpy.loadtxt("vectors/positive.csv",  delimiter=',')
 negetive = numpy.loadtxt("vectors/negetive.csv", delimiter=',')
 X = numpy.concatenate((positive, negetive), axis=0)
 p = numpy.ones((no_samples, 1))
-n = numpy.full((no_samples, 1), -1)
+n = numpy.full((no_samples, 1), -1, dtype=numpy.int64)
 Y = numpy.concatenate((p,n), axis=0)
 y = Y.ravel()
 #scale
@@ -23,12 +24,13 @@ scaler = preprocessing.StandardScaler().fit(X)
 svm_module = svm.SVC()
 classifier = Pipeline(steps= [('svm', svm_module)]) #[('scale', scaler), ('svm', svm_module)])
 classifier.fit(X, y)
+print "Classifer created"
 ##############################################################
 
 ######################bhargavi part###########################
 def isclickbait(document):
 	try:
-		title_vector = utility.create_vector(document)
+		title_vector = numpy.array(utility.create_vector(document)).reshape(1,-1)
 		# t = scaler.transform(title_vector)
 		prediction = classifier.predict(title_vector)
 		if prediction[0] == 1:
@@ -44,6 +46,8 @@ def isclickbait(document):
 if __name__ == '__main__':
 	input_text = "W"
 	while input_text != "Q":
-		input_text = input("Enter a clickbait title")
+		input_text = raw_input("Enter a clickbait title or 'Q/q' to exit: ")
+		if input_text == "Q" or input_text == 'q':
+			break
 		isclickbait(input_text)
 	print "Exited Succesfully"
