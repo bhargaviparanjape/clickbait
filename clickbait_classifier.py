bb2bf2361740ccc43a0be5c059d2867ecebda7eb
abhijnanc <abhijnan.iitkgp@gmail.com>
2016-12-15 16:25:12 +0530

    Update and rename server.py to clickbait_classifier.py

--- clickbait_classifier.py
+++ clickbait_classifier.py
@@ -6,13 +6,11 @@ import subprocess
 import time
 import utility
 
-
-
 ###############Classifier############################
 print "creating classifier..."
 no_samples = 10000
 positive = numpy.loadtxt("vectors/positive.csv",  delimiter=',')
-negetive = numpy.loadtxt("vectors/negetive.csv", delimiter=',')
+negetive = numpy.loadtxt("vectors/negative.csv", delimiter=',')
 X = numpy.concatenate((positive, negetive), axis=0)
 p = numpy.ones((no_samples, 1))
 n = numpy.full((no_samples, 1), -1, dtype=numpy.int64)
@@ -38,15 +36,12 @@ def isclickbait(document):
 		else:
 			print "That's not Clickbait!"
 	except Exception,e:
-		print "except:"
-		print e
-
-
+		print "except:", e
 
 if __name__ == '__main__':
 	input_text = "W"
 	while input_text != "Q":
-		input_text = raw_input("Enter a clickbait title or 'Q/q' to exit: ")
+		input_text = raw_input("Enter a news headline or 'Q/q' to exit: ")
 		if input_text == "Q" or input_text == 'q':
 			break
 		isclickbait(input_text)
